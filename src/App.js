cace7d0254833e7638456343acde9672e8846be5
Sean Sutherland <SeanRSuther@gmail.com>
2020-05-04 13:09:50 -0700

    Mostly working relative date

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import './App.css';
-import SocialCard from './SocialCard';
+import React from "react";
+import "./App.css";
+import SocialCard from "./SocialCard";
+
+const me = {
+  username: "srsutherland",
+  displayname: "Sean Sutherland",
+  avatarURL: "https://avatars3.githubusercontent.com/u/12262958",
+};
+
+const comment1 = {
+  user: me,
+  comment: "According to all known laws of aviation, the honeybee",
+  date: Date.now(),
+  replies: 0,
+  likes: 0,
+  shares:0,
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SocialCard />
+        <SocialCard post={comment1} user={me} />
       </header>
     </div>
   );
--- src/SocialCard.js
+++ src/SocialCard.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+const defaultUser = {
+  username: "[deleted]",
+  displayname: "Deleted User",
+  avatarURL: "logo.svg",
+};
+
 class SocialCard extends React.Component {
   constructor(props) {
     super(props);
@@ -11,12 +17,61 @@ class SocialCard extends React.Component {
   }
 
   render() {
+    const user = this.props.user || this.props.post.user || defaultUser;
+    const date = this.props.post.date;
+    let dateline;
+    if (date) {
+      const shorttime = this.getRelativeTime(date);
+      const fulltime = this.props.post.date.toLocaleString(date);
+      dateline = <span class="time" title={fulltime}>{` · ${shorttime}`}</span>;
+    }
+
     return (
-      <div>
-        <div>{this.props.username?this.props.username:"[deleted]"}:</div>
-        <div>I'm a widdle baby</div>
+      <div class="socialcard">
+        <div>
+          <span class="displayname">{user.displayname || "Unknown"}</span>
+          <span class="displayname"> @{user.username || "unknown"}</span>
+          {dateline}
+        </div>
+    <div>{this.props.post.comment}</div>
       </div>
     );
+  }
+
+  getRelativeTime(date) {
+    const now = Date.now();
+
+    if (!date) {
+      return "";
+    }
+    if (date > now) {
+      return "in the future";
+    }
+    const seconds = (now - date) / 1000; //seconds
+    if (seconds < 60) {
+      return `${Math.ceil(seconds)}s ago`;
+    }
+    const minutes = seconds / 60; //minutes
+    if (minutes < 60) {
+      return `${Math.ceil(minutes)}m ago`;
+    }
+    const hours = minutes / 60; //hours
+    if (hours < 24) {
+      return `${Math.ceil(hours)}h ago`;
+    }
+    const days = hours / 24; //days
+    if (days < 7) {
+      return `${Math.ceil(days)}d ago`;
+    }
+    if (days < 365) {
+      return date.toLocaleString("default", { month: "short", day: "numeric" });
+    } else {
+      return date.toLocaleString("default", {
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+      });
+    }
   }
 }
 
